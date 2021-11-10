0387342b187c2ee40009c0ac6a747f7ff296ebd3
Pedro Arthur <pedro.afd@discente.ufma.br>
2021-11-10 08:26:20 -0300

    primeira api

---
+++ https/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace https.Controllers;
+
+[ApiController] //Atributo: habilita comportamentos dogmáticos
+[Route("[controller]")] //Define o padrão de roteamento 
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")] //este atributo encaminha as solicitações para IEnumerable<WeatherForecast> Get()
+    //um controlador de API só precisa implementar uma ação de controlador, conforme visto neste exemplo.
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
