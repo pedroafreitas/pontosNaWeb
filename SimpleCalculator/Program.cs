83c3fbf4316c5a91d0f260cc9ca08f05d2913b4b
Pedro Arthur <pedro.afd@discente.ufma.br>
2021-11-12 08:31:23 -0300

    initializing classes

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -16,6 +16,54 @@ namespace SimpleCalculator
 
             Console.WriteLine(result);
 
+            string someText = "meh";
+            string otherText = "meh2";
+
+            bool isEqual = someText.Equals(otherText, StringComparison.Ordinal);
+
+            string addedText = someText + " " + otherText + " meh 3";
+            string formatedText = string.Format("{0} {1} meh3", someText, otherText);
+
+            Console.WriteLine(formatedText.Length);
+            Console.WriteLine(addedText.Length);
+            Console.WriteLine(formatedText[3]);
+
+            Console.WriteLine(someText.Substring(0,2));
+
+            Console.WriteLine(someText.ToLower());
+            Console.WriteLine(someText.ToUpper());
+
+            string anotherText = string.Empty;
+
+            string replacedText = someText.Replace("h", "lhor emprego do mundo");
+            Console.WriteLine(replacedText);
+
+            input = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            if (input.Equals("Pedro") && password.Equals("123"))
+            {
+                Console.WriteLine("nice");
+            } else if (input.Equals("Anna") && password.Equals("123"))
+            {
+                Console.WriteLine("nice");
+            }
+            else
+            {
+                return 1;
+            }
+
+            switch(input)
+            {
+                case "Pedro":
+                    Console.WriteLine("nice");
+                    break;
+                case "Anna":
+                    Console.WriteLine("nice");
+                    break; 
+                default:
+                    return 1;
+            }
             return 0;
         }
     }
