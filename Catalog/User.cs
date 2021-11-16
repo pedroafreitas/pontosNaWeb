cdcf5582b416528051d66a804417a1ef823c800f
Pedro Arthur <pedro.afd@discente.ufma.br>
2021-11-16 10:35:55 -0300

    mail api

---
+++ Catalog/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class User
+{
+    public Guid Guid { get; set; }
+    public string Email {get; set; }
+    public string Password {get; set;}
+    public string Name {get; set;}
+
+    public User()
+    {
+
+    }
+}
