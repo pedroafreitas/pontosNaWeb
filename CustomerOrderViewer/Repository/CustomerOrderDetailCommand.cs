bce265ed2799228296570fc9f708653667cf925f
Pedro Arthur Freitas <pedro.afd@discente.ufma.br>
2021-11-23 14:47:23 -0300

    first db project done

--- CustomerOrderViewer/Repository/CustomerOrderDetailCommand.cs
+++ CustomerOrderViewer/Repository/CustomerOrderDetailCommand.cs
@@ -1,5 +1,3 @@
-
-
 using System.Data.SqlClient;
 using CustomerOrderViewer.Models;
 
@@ -14,23 +12,35 @@ namespace CustomerOrderViewer.Repository
             _connectionString = connectionString;
         }
 
+        //Connection, Command, Reader
         public IList<CustomerOrderDetailModel> GetList()
         {
             List<CustomerOrderDetailModel> customerOrderDetailModels = new();
 
+            //The connection will be disposed after we use the connection
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
-            
-                using(SqlCommand command = new SqlCommand("SELECT CustomerOrderId, CustomerId, ItemId, FirstName, LastName, [Description], Price From CustomerOrderDetail", connection))
+                //Cria um comando
+                using(SqlCommand command = new SqlCommand(@"SELECT 
+                                                                CustomerOrderId, 
+                                                                CustomerId, ItemId, 
+                                                                FirstName, 
+                                                                LastName, 
+                                                                [Description], 
+                                                                Price 
+                                                            From CustomerOrderDetail", connection))
                 {
+
+                    //The reader object helps us read the db output
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         if (reader.HasRows)
                           {
+                            //While there are records, you read.
                             while (reader.Read())
                             {
-                                CustomerOrderDetailModel customerOrderDetailModel = new CustomerOrderDetailModel()
+                                CustomerOrderDetailModel customerOrderDetailModel = new()
                                 {
                                     CustomerOrderId = Convert.ToInt32(reader["CustomerOrderId"]),
                                     CustomerId = Convert.ToInt32(reader["CustomerId"]),
