38d79f2fe099e36038578a39609f43a8cc5b8c55
Pedro Arthur Freitas <pedro.afd@discente.ufma.br>
2021-11-30 08:41:15 -0300

    controller done

--- OneMoreApi/Controllers/UsuariosController.cs
+++ OneMoreApi/Controllers/UsuariosController.cs
@@ -33,5 +33,48 @@ namespace OneMoreApi.Controllers
 
             return new ObjectResult(usuario);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Usuario usuario)
+        {
+            if(usuario == null)
+                return BadRequest();
+
+            _usuarioRepository.add(usuario);
+
+            return CreatedAtRoute("GetUsuario", new {id = usuario.UsuarioId}, usuario);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Usuario usuario)
+        {
+            if(usuario == null || usuario.UsuarioId != id)
+                return BadRequest();
+            
+            var _usuario = _usuarioRepository.Find(id);
+
+            if(usuario == null)
+                return NotFound();
+
+            _usuario.Nome = usuario.Nome;
+            _usuario.Email = usuario.Email;
+
+            _usuarioRepository.Update(_usuario);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var usuario = _usuarioRepository.Find(id);
+
+            if(usuario == null)
+                return NotFound();
+
+            _usuarioRepository.Remove(id);
+            
+            return new NoContentResult();
+        }
+
     }
 }
