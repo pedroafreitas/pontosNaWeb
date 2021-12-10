8680444d375dc0e5569ed5f796baa4e05cea6645
pedroafreitas <pedro.afd@discente.ufma.br>
2021-12-10 17:14:53 -0300

    adding

--- CodeSnippetsSolution/test/CommandAPI.Tests/UnitTest1.cs
+++ CodeSnippetsSolution/test/CommandAPI.Tests/UnitTest1.cs
@@ -1,12 +1,14 @@
+using System;
 using Xunit;
 
-namespace CommandAPI.Tests;
-
-public class UnitTest1
+namespace CommandAPI.Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void Test1()
+        {
 
+        }
     }
 }
