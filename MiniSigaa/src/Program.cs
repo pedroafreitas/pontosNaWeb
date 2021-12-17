aaf84436be679c0cc633bcc3b7694cbc7d0ae4e4
Pedro Arthur Freitas <pedro.afd@discente.ufma.br>
2021-12-17 10:10:59 -0300

    main menu

--- MiniSigaa/src/Program.cs
+++ MiniSigaa/src/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MiniSigaa.Models;
+using MiniSigaa.Models.Repository;
+
+namespace MiniSigaa
+{
+    public class Program
+    {
+        private readonly ICourseClassRepository _courseClasses;
+        public Program(ICourseClassRepository courseClass)
+        {
+            _courseClasses = courseClass;
+        }
+
+        public static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+                showMenu = MainMenu();
+        }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Bem-vindo(a) ao Mini Sigaa");
+
+            Console.WriteLine("1 - Listar Turmas Existentes");
+            Console.WriteLine("2 - Cadastrar Notas de um Aluno");
+            Console.WriteLine("Aperte 0 para sair");
+            
+            switch (Console.ReadLine())
+            {
+                case "0":
+                    Console.WriteLine("Terminando programa...");
+                    return false;
+                case "1":
+                    Console.WriteLine("\nTurmas existentes:");
+                    ListCourseClasses();
+                    return true;
+                case "2":
+                    RegisterGrades();
+                    return true;
+                default:
+                    return true;
+            }
+        }
+
+        private static void RegisterGrades()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ListCourseClasses()
+        {
+            throw new NotImplementedException();            
+        }
+    }
+}
