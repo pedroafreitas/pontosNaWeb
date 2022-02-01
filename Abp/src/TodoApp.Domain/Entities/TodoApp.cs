4509a25dcadece7e9da942f46b4170a91b4cea21
Pedro Arthur Freitas <pedro.afd@discente.ufma.br>
2022-02-01 10:07:37 -0300

    starting abp

---
+++ Abp/src/TodoApp.Domain/Entities/TodoApp.cs
@@ -0,0 +1,11 @@
+using System;
+
+using Volo.Abp.Domain.Entities;
+
+namespace TodoApp
+{
+    public class TodoItem : BasicAggregateRoot<Guid>
+    {
+        public string Text {get; set; }
+    }
+}
