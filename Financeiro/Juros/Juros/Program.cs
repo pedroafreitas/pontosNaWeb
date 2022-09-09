02fce12d3a8e4929c43f1bd483968e337946f132
Pedro Arthur Freitas <pedro.afd@discente.ufma.br>
2022-09-09 11:08:07 -0300

    feat: calculo tabela Price

--- Financeiro/Juros/Juros/Program.cs
+++ Financeiro/Juros/Juros/Program.cs
@@ -1,60 +1,83 @@
-﻿public class Program
+﻿using Extreme.Mathematics;
+
+public class Program
 {
 	public static void Main()
 	{
 		ImprimirParcelas(Sac(80000, 80, 2));
+		ImprimirParcelas(Price(80000, 80, 2));
 	}
 
-	public static List<ParcelaSac> Sac(decimal saldoDevedor, int prazo, decimal jurosAoAno)
+    private static List<Parcela> Sac(decimal saldoDevedor, int prazo, decimal TaxaDeJurosAoAno)
 	{
-		var parcelas = new List<ParcelaSac>();
-		var amortizacao = saldoDevedor / prazo;
-		var jurosAoMes = (jurosAoAno/100) / 12;
+		var parcelas = new List<Parcela>();		
+		var jurosAoMes = (TaxaDeJurosAoAno / 100) / 12;
 		while (saldoDevedor > 0)
 		{ 
-			var juros = saldoDevedor * jurosAoMes; 
+			var juros = saldoDevedor * jurosAoMes;
+			var amortizacao = saldoDevedor / prazo;
 			parcelas.Add(
-				new ParcelaSac(prazo, amortizacao, saldoDevedor, juros, amortizacao + juros)
+				new Parcela(prazo, amortizacao, saldoDevedor, juros, amortizacao + juros)
 			);
 			saldoDevedor -= amortizacao;
 			prazo -= 1;
 		}
-
+		return parcelas;
+	}
+	
+	private static List<Parcela> Price(decimal saldoDevedor, int prazo, decimal TaxaDeJurosAoAno)
+    {		
+		var parcelas = new List<Parcela>();
+		decimal jurosAoMes = (TaxaDeJurosAoAno / 100) / 12;
+		decimal prazoDecimal = prazo;
+        var prestacao = saldoDevedor * 
+			((DecimalMath.Pow((1 + jurosAoMes), prazoDecimal) * jurosAoMes) / 
+			(DecimalMath.Pow((1 + jurosAoMes), prazoDecimal) - 1));
+		while(saldoDevedor > 0)
+        {
+			var juros = saldoDevedor * jurosAoMes;
+			var amortizacao = prestacao - juros;
+			parcelas.Add(
+				new Parcela(prazo, amortizacao, saldoDevedor, juros, prestacao)
+			);
+			saldoDevedor -= amortizacao;
+			prazo -= 1;
+		}
 		return parcelas;
 	}
 
-	public static void ImprimirParcelas(List<ParcelaSac> parcelas)
+	private static void ImprimirParcelas(List<Parcela> parcelas)
 	{
-		foreach (ParcelaSac parcela in parcelas)
+		foreach (Parcela parcela in parcelas)
 		{
 			Console.WriteLine(
-				"Num: " + parcela.prazo
-				+ "| SD: " + parcela.saldoDevedor.ToString("#.##")
-				+ "| Amort: " + parcela.amortizacao.ToString("#.##")
-				+ "| Juros: " + parcela.juros.ToString("#.##")
-				+ "| Prestacao: " + parcela.prestacao.ToString("#.##"));
+				"Num: " + parcela.Prazo
+				+ "| SD: " + parcela.SaldoDevedor.ToString("#.##")
+				+ "| Amort: " + parcela.Amortizacao.ToString("#.##")
+				+ "| Juros: " + parcela.Juros.ToString("#.##")
+				+ "| Prestacao: " + parcela.Prestacao.ToString("#.##"));
 		}
 	}
 }
 
-public class ParcelaSac
+public class Parcela
 {
-	public int prazo { get; set; }
-	public decimal amortizacao { get; set; }
-	public decimal saldoDevedor { get; set; }
-	public decimal juros { get; set; }
-	public decimal prestacao { get; set; }
+	public int Prazo { get; set; }
+	public decimal Amortizacao { get; set; }
+	public decimal SaldoDevedor { get; set; }
+	public decimal Juros { get; set; }
+	public decimal Prestacao { get; set; }
 
-    public ParcelaSac(int Parcela, 
+    public Parcela(int Parcela, 
 		decimal Amortizacao,
 		decimal SaldoDevedor,
 		decimal Juros, 
 		decimal Prestacao) 
 	{
-		prazo = Parcela;
-		amortizacao = Amortizacao;
-		saldoDevedor = SaldoDevedor;
-		juros = Juros;
-		prestacao = Prestacao;
+		Prazo = Parcela;
+		this.Amortizacao = Amortizacao;
+		this.SaldoDevedor = SaldoDevedor;
+		this.Juros = Juros;
+		this.Prestacao = Prestacao;
 	}
 }
